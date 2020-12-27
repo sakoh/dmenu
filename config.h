8a47c044a706677e28e5c261eae726e38d3f902a
Sako Hartounian <sakohartounian@yahoo.com>
2020-12-26 21:49:22 -0800

    dmenu themed to my desktop

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
+	"Font Awesome 5 Free Solid:pixelsize=18"
+	"Fira Mono Nerd Font:pixelsize=18",
 	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
@@ -12,8 +13,8 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#8fbcbb", "#101726" },
+	[SchemeSel] = { "#101726", "#8fbcbb" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
